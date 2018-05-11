9a5b1719b3da2bee6736960f6db86b1bdf9207f2
ote <wdxc1992@qq.com>
2018-05-12 01:06:31 +0800

    dongtai

---
+++ app/src/main/java/com/example/schoolIM/ui/ActActivity.java
@@ -0,0 +1,15 @@
+package com.example.schoolIM.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hyphenate.schoolIM.R;
+
+public class ActActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act);
+    }
+}
